379b3b51a8765a5be7a879697825ca5323e32e64
Fleli <frederikedvardsen@gmail.com>
2023-12-30 15:44:05 +0100

    Added DEBUG compile-time option via macros

--- src/DataStructures/Array.c
+++ src/DataStructures/Array.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "../meta.h"
+
 #include "Array.h"
 
 Array *array_init(void) {
@@ -10,10 +12,12 @@ Array *array_init(void) {
 
 Array *array_init_with_initial_capacity(int initialCapacity) {
     
+    #ifdef DEBUG
     if (initialCapacity <= 0) {
         printf("Cannot initialize array of size %d <= 0.\n", initialCapacity);
         exit(1);
     }
+    #endif
     
     Array *a = malloc( sizeof(Array) );
     
@@ -29,10 +33,12 @@ Array *array_init_with_initial_capacity(int initialCapacity) {
 // The insert method automatically adjusts the array size after insertion if this invariant fails.
 void array_insert(Array *a, void *newElement, int index) {
     
+    #ifdef DEBUG
     if (index < 0 || index > a->count) {
         printf("Cannot insert element at index %d, with array count %d.\n", index, a->count);
         exit(1);
     }
+    #endif
     
     // Move all elements after the insertion index one place to the right.
     int newLocation = a->count;
@@ -58,10 +64,12 @@ void array_append(Array *a, void *newElement) {
 
 void* array_get(Array *a, int index) {
     
+    #ifdef DEBUG
     if (index < 0 || index >= a->count) {
         printf("Index %d out of bounds for array with count %d.\n", index, a->count);
         exit(1);
     }
+    #endif
     
     return (a->data)[index];
     
