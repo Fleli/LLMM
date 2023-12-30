d6e78c6dedcbbdf2ec0c974f1e618bd5174500fd
Fleli <frederikedvardsen@gmail.com>
2023-12-30 01:16:21 +0100

    Added LLMM::Function class

---
+++ Internal/Function.hpp
@@ -0,0 +1,20 @@
+
+#include "FunctionAttribute.hpp"
+
+using namespace LLMM;
+
+namespace LLMM
+{
+    
+    class Function {
+        
+    public:
+        
+        
+    private:
+        /* Indicates that this function never writes to memory through a pointer */
+        bool noLHSPointer;
+        
+    };
+    
+} // namespace LLMM
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Low-Level Mediocre Machine (LLMM)
 
-Please read `LLMM/IR` for a thorough guide of the intermediate representation used in LLMM.
+Please read `LLMM/Documentation` for a thorough guide of the intermediate representation used in LLMM.
